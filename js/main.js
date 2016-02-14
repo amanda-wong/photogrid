6e0580a41b47f775ff8fef6d6710f0dd2055c7e1
emilie-nagahama <emilie.nagahama@gmail.com>
2016-02-14 12:50:07 -0800

    tried to read the min.main.js file from the index but was unsuccessful. Added an IF statement to around the .each loop so that the error message shows if the input is submitted without content

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 
 $(function() {
 
+	var igData;
 	var result = " ";
 	var next_url = " ";
 	var $display = $('.grid');
@@ -28,24 +29,33 @@ $(function() {
 		})
 
 		.done(function(instaData) {
+
+			igData=instaData.data; // assign the function to data in the variable igData for the upcoming if statement
+
 			result = ' ';
 			$('header').height('auto');
 			$('.loading-image').fadeOut();
-			$.each(instaData.data, function(index, value) {
-				result += '<li class="ig-photo-wrap">';
-				result += '<a target="_blank" href="' + value.link + '"><img class="ig-image" src=' + value.images.standard_resolution.url + '></a>';
-				result += '<div class="ig-transparent">';
-				result += '<div class="profile-pic-wrap"><img src=' + value.user.profile_picture + '></div>';
-				result += '<div class="ig-details-wrap">';
-				result += '<div class="username">' + value.user.username + '</div>';
-				result += '<div class="comments-likes-wrap"><i class="fa fa-comments"></i><p>' + value.comments.count + '</p><i class="fa fa-heart"></i><p>' + value.likes.count + '</p></div>';
-				result += '</div></div></li>';
-			});
 
-			next_url = instaData.pagination.next_url;
-			$('.load-more-button').show();
+			if (igData.length !== 0) {
+				$.each(instaData.data, function(index, value) {
+					result += '<li class="ig-photo-wrap">';
+					result += '<a target="_blank" href="' + value.link + '"><img class="ig-image" src=' + value.images.standard_resolution.url + '></a>';
+					result += '<div class="ig-transparent">';
+					result += '<div class="profile-pic-wrap"><img src=' + value.user.profile_picture + '></div>';
+					result += '<div class="ig-details-wrap">';
+					result += '<div class="username">' + value.user.username + '</div>';
+					result += '<div class="comments-likes-wrap"><i class="fa fa-comments"></i><p>' + value.comments.count + '</p><i class="fa fa-heart"></i><p>' + value.likes.count + '</p></div>';
+					result += '</div></div></li>';
+				});
 
-			$display.append(result);
+				next_url = instaData.pagination.next_url;
+				$('.load-more-button').show();
+
+				$display.append(result);
+
+			} else {
+				result += '<p>please enter a hashtag to continue</p>';
+			}
 		})
 
 		.fail(function(argument) {
